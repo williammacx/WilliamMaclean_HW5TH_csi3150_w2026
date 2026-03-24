71f3c03f7ccbccf8a158d46b16ae0f11049493b2
williammacx <williammacx@gmail.com>
2026-03-24 13:51:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,5 +2,9 @@ const carList = document.getElementById("carlist");
 const message = document.getElementById("message");
 
 function makeFilters() {
+  const makeSelect = document.getElementById("make");
+  const colorSelect = document.getElementById("color");
+
+  const make = [...new Set usedCars.map(car => car.make))];
 
 };
